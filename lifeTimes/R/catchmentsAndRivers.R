4c3c874ce6e99037c1ff813ff206dffe145f56a0
Lucas Dent <lucasdent@CloMt-2.local>
2022-01-09 04:03:42 +0000

    fully refactored code

---
+++ lifeTimes/R/catchmentsAndRivers.R
@@ -0,0 +1,14 @@
+#' Rainfall and riverflow data for two separate catchment regions.
+#'
+#' A dataframe with yearly rainfall_cm and riverflow, for the Ash river at Mardock,
+#' and the Thames river at Kingston in the United Kingdom
+#'
+#' @format a dataframe with yearly rainfall_cm and riverflow, for the Ash river at Mardock,
+#' and the Thames river at Kingston in the United Kingdom:
+#' \describe{
+#'   \item{season}{summer, autumn, winter, spring}
+#'   \item{catchmentRegion}{Ash at Mardock, or, Thames at Kingston}
+#'   ...
+#' }
+#' @source \url{https://nrfa.ceh.ac.uk//}
+"catchmentsAndRivers"
