1a9dc1b81717d48ff6627e0044e34ebce5f07125
Lucas Dent <lucasdent@CloMt-2.local>
2022-02-04 12:31:15 +0000

    unit tests incorporated

--- tests/testthat/test-lts_input.R
+++ tests/testthat/test-lts_input.R
@@ -3,7 +3,7 @@
 # library(lifeTimes)
 
 test_that("lts_input returns a list", {
-  expect_true(is.list(eval(lts_input())))
+  expect_true(is.list(eval(lifeTimes::lts_input())))
   # expect_equal(str_length("ab"), 2)
   # expect_equal(str_length("abc"), 3)
 })
--- tests/testthat/test-lts_internalFunctions_generalised_kmReplace.R
+++ tests/testthat/test-lts_internalFunctions_generalised_kmReplace.R
@@ -3,7 +3,7 @@
 test_that("dimensions of wide dataframe are correct based on variables input", {
 
   # lts_default <- lifeTimes:::dev_lts_inputNoCalls()
-  lts_userInput <- lts_input()
+  lts_userInput <- lifeTimes::lts_input()
   lts_data <- lts_userInput$lts_variables$lts_data
   lts_num_unique_obs <- length(unique(lts_data[,lts_userInput$lts_variables$lts_uniqueID_colname]))
   lts_num_features_to_compare <- length(unlist(lts_userInput$lts_variables$lts_pariedComparisons))
