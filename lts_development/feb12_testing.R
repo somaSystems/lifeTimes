3d438783a55344a3744a21bca9858a7dfb80c161
Lucas Dent <lucasdent@CloMt-2.local>
2022-02-20 12:18:16 +0000

    corrections to 'pivot_wider' ID_columns argument, which appeared to break following tidyr update

--- lts_development/feb12_testing.R
+++ lts_development/feb12_testing.R
@@ -1,31 +1,31 @@
 #testing script
 library(lifeTimes)
-lts <- lifeTimes:::dev_lts_inputNoCalls
-lts
-lts_in <- function(.in_tsData = catchmentsAndRivers,
-                   .in_time = c("dayOfseason"),
-                   .in_compare_categorical = c("season","catchmentRegion"), #Categorical variables
-                   .in_plot_measured_variables = FALSE,
-                   .in_pairedComparisons = list(
-                     pair_1 =list(x ="flow_m3s" , y ="rainfall_cm" )), #pairedVarCCF
-                   .in_uniqueID_colname = "key_num",
-                   .in_metaData = NULL){
-
-  lts_inputVars <- lts_input(.tsData = .in_tsData,
-                             .time = .in_time,
-                             .compare_categorical = .in_compare_categorical, #Categorical variables
-                             .plot_measured_variables =.in_plot_measured_variables,
-                             .pairedComparisons = .in_pairedComparisons, #pairedVarCCF
-                             .uniqueID_colname = .in_uniqueID_colname,
-                             .metaData = .in_metaData )
-
-  lts_tsToWide(lts_inputVars) %>%
-    lts_wide_ts_to_ccf(.lts_variables = lts_inputVars) %>%
-    lts_ccf_df(.lts_variables = lts_inputVars) %>%
-    lts_metaData_ccf_join(.lts_variables = lts_inputVars) %>%
-    lts_summarise_ccf(.lts_variables = lts_inputVars) %>%
-    lts_cluster_ccf_summs(.lts_variables = lts_inputVars) -> lts_Output
-}
+lts <- lifeTimes:::dev_lts_inputNoCalls()
+# lts
+# lts_in <- function(.in_tsData = catchmentsAndRivers,
+#                    .in_time = c("dayOfseason"),
+#                    .in_compare_categorical = c("season","catchmentRegion"), #Categorical variables
+#                    .in_plot_measured_variables = FALSE,
+#                    .in_pairedComparisons = list(
+#                      pair_1 =list(x ="flow_m3s" , y ="rainfall_cm" )), #pairedVarCCF
+#                    .in_uniqueID_colname = "key_num",
+#                    .in_metaData = NULL){
+#
+#   lts_inputVars <- lts_input(.tsData = .in_tsData,
+#                              .time = .in_time,
+#                              .compare_categorical = .in_compare_categorical, #Categorical variables
+#                              .plot_measured_variables =.in_plot_measured_variables,
+#                              .pairedComparisons = .in_pairedComparisons, #pairedVarCCF
+#                              .uniqueID_colname = .in_uniqueID_colname,
+#                              .metaData = .in_metaData )
+#
+#   lts_tsToWide(lts_inputVars) %>%
+#     lts_wide_ts_to_ccf(.lts_variables = lts_inputVars) %>%
+#     lts_ccf_df(.lts_variables = lts_inputVars) %>%
+#     lts_metaData_ccf_join(.lts_variables = lts_inputVars) %>%
+#     lts_summarise_ccf(.lts_variables = lts_inputVars) %>%
+#     lts_cluster_ccf_summs(.lts_variables = lts_inputVars) -> lts_Output
+# }
 
 lts_wide <- lifeTimes:::lts_tsToWide(lts)
 lts_ccf <- lifeTimes:::lts_wide_ts_to_ccf(.lts_cast_ts = lts_wide ,.lts_variables = lts )
