132a4eccea9a6d7ec7c628a191d08a4932c24bd9
L Dent <12620779+somaSystems@users.noreply.github.com>
2024-12-29 21:16:38 +0000

    updated Refactor data handling and lts_in function
    - Changed default data to load from the data directory.
    - Removed ; consolidated functionality into .
    - Refactored  to remove pipes for clarity.
    - Updated  to handle categorical variables as factors and unique ID column as character.
    - Ensured  is coerced to a dataframe.
    - Tested changes and confirmed functionality remains intact.

--- R/lts_in.R
+++ R/lts_in.R
@@ -1,45 +1,71 @@
-#' lts_calc
-#'
-#' @param .in_tsData tidy time series data
-#' @param .in_time name of the "time" variable
-#' @param .in_compare_categorical names of categorical or explanatory variables to compare CCFs by. In future releases of this function, if there is only one explanatory variable, it will be possible to include a key of set of ".pairedComparisons".
-#' @param .in_plot_measured_variables logical parameter, set to TRUE if using one categorical variables and want different CCFs plotted against a single categorical variable.
-#' @param .in_pairedComparisons a single pair or list of pairs, of names of variables to generate cross correlations for.
-#' @param .in_uniqueID_colname name of column with unique identifier
-#' @param .in_lagMax maximum lag in CCFs
-#' @param .in_clusterByPortions defaults to cluster by mean correlation at mode maximum correlated lag, otherwise clusters by "portion" that each grouping/facet of data represents as a total of category 1.
-#' @param .in_metaData name of columns with metaData
-#' @return calculated cross correlations, summary statistics and clustering that can be used for classification or plotting
-#'
-#' @export
-#'
+lts_in <- function(
+    .in_tsData = NULL,
+    .in_time = c("dayOfseason"),           # Time variable
+    .in_compare_categorical = c("season", "catchmentRegion"), # Categorical variables
+    .in_plot_measured_variables = FALSE,   # Option to plot measured variables
+    .in_pairedComparisons = list(pair_1 = list(y = "flow_m3s", x = "rainfall_cm")), # Variable pairs for CCF
+    .in_uniqueID_colname = "key_num",      # Column for unique IDs
+    .in_lagMax = NULL,                     # Maximum lag for CCF
+    .in_clusterByPortions = FALSE,         # Cluster by portions or mode max correlation
+    .in_metaData = NULL,                   # Metadata columns
+    return_intermediate = FALSE            # Option to return intermediate results
+) {
+  # Dynamically load default dataset if .in_tsData is not provided
+  if (is.null(.in_tsData)) {
+    data("lts_catchmentsAndRivers", package = "lifeTimes", envir = environment())
+    .in_tsData <- lts_catchmentsAndRivers
+  }
 
-lts_in <- function(.in_tsData = lts_catchmentsAndRivers,
-                       .in_time = c("dayOfseason"),
-                       .in_compare_categorical = c("season","catchmentRegion"), #Categorical variables
-                       .in_plot_measured_variables = FALSE,
-                       .in_pairedComparisons = list(
-                         pair_1 =list(y ="flow_m3s",x ="rainfall_cm")), #pairedVarCCF
-                       .in_uniqueID_colname = "key_num",
-                        .in_lagMax = NULL, #hotfix July 27 2022,
-                       .in_clusterByPortions = FALSE, #hotfix added August 2 2022
-                       .in_metaData = NULL
-                        ){
+  # Ensure .in_tsData is a data frame
+  .in_tsData <- as.data.frame(.in_tsData)
 
-  lts_inputVars <- lts_input(.tsData = .in_tsData,
-                             .time = .in_time,
-                             .compare_categorical = .in_compare_categorical, #Categorical variables
-                             .plot_measured_variables =.in_plot_measured_variables,
-                             .pairedComparisons = .in_pairedComparisons, #pairedVarCCF
-                             .uniqueID_colname = .in_uniqueID_colname,
-                             .lagMax = .in_lagMax,  #hotfix July 27 2022
-                             .clusterByPortions = .in_clusterByPortions, #hotfix added August 2 2022
-                             .metaData = .in_metaData )
+  # Convert specified columns to factors or characters as required
+  if (!is.null(.in_compare_categorical)) {
+    .in_tsData[.in_compare_categorical] <- lapply(
+      .in_tsData[.in_compare_categorical],
+      as.factor
+    )
+  }
+  if (!is.null(.in_uniqueID_colname)) {
+    .in_tsData[.in_uniqueID_colname] <- lapply(
+      .in_tsData[.in_uniqueID_colname],
+      as.character
+    )
+  }
 
-  lts_tsToWide(lts_inputVars) %>%
-  lts_wide_ts_to_ccf(.lts_variables = lts_inputVars) %>%
-  lts_ccf_df(.lts_variables = lts_inputVars) %>%
-  lts_metaData_ccf_join(.lts_variables = lts_inputVars) %>%
-  lts_summarise_ccf(.lts_variables = lts_inputVars) %>%
-  lts_cluster_ccf_summs(.lts_variables = lts_inputVars) -> lts_Output
+  # Create input variable list
+  lts_inputVars <- list(
+    lts_data = .in_tsData,
+    lts_time = .in_time,
+    lts_compare_by = .in_compare_categorical,
+    lts_plot_measured_variables = .in_plot_measured_variables,
+    lts_pariedComparisons = .in_pairedComparisons,
+    lts_uniqueID_colname = .in_uniqueID_colname,
+    lts_lagMax = .in_lagMax,
+    lts_clusterByPortions = .in_clusterByPortions,
+    lts_metaData = .in_metaData
+  )
+
+  # Stepwise execution of the pipeline
+  wide_data <- lts_tsToWide(lts_inputVars)
+  ccf_data <- lts_wide_ts_to_ccf(wide_data, .lts_variables = lts_inputVars)
+  ccf_df <- lts_ccf_df(ccf_data, .lts_variables = lts_inputVars)
+  ccf_with_meta <- lts_metaData_ccf_join(ccf_df, .lts_variables = lts_inputVars)
+  ccf_summary <- lts_summarise_ccf(ccf_with_meta, .lts_variables = lts_inputVars)
+  lts_Output <- lts_cluster_ccf_summs(ccf_summary, .lts_variables = lts_inputVars)
+
+  # Optionally return all intermediate results
+  if (return_intermediate) {
+    return(list(
+      wide_data = wide_data,
+      ccf_data = ccf_data,
+      ccf_df = ccf_df,
+      ccf_with_meta = ccf_with_meta,
+      ccf_summary = ccf_summary,
+      final_output = lts_Output
+    ))
+  }
+
+  # Otherwise, return only the final output
+  return(lts_Output)
 }
--- R/lts_input.R
+++ R/lts_input.R
@@ -7,7 +7,7 @@
 #' @param .compare_categorical names of categorical or explanatory variables to compare CCFs by. In future releases of this function, if there is only one explanatory variable, it will be possible to include a key of set of ".pairedComparisons".
 #' @param .pairedComparisons a single pair or list of pairs, of names of variables to generate cross correlations for.
 #' @param .plot_measured_variables logical parameter, set to TRUE if using one categorical variables and want different CCFs plotted against a single categorical variable.
-#' @param .uniqueID_colname name of colum with unique identifier
+#' @param .uniqueID_colname name of column with unique identifier
 #' @param .lagMax maximum lag in CCFs
 #' @param .clusterByPortions defaults to cluster by mean correlation at mode maximum correlated lag, otherwise clusters by "portion" that each grouping/facet of data represents as a total of category 1.
 #' @param .metaData name of columns with metaData
@@ -15,55 +15,47 @@
 #' @return a list that includes time series data, and strings from user input that map variables in the time series data to input in lifeTimes functions. Eg. which column of dataframe is the unit of "time", which is the categorical variables, and which are the variables to compare when generating CCFs.
 #' @export
 
+lts_input <- function(
+    .tsData,
+    .time = NULL,
+    .compare_categorical = NULL, # Categorical variables
+    .plot_measured_variables = FALSE,
+    .pairedComparisons = NULL, # pairedVarCCF
+    .uniqueID_colname = NULL,
+    .lagMax = NULL, # hotfix July 27 2022
+    .clusterByPortions = FALSE, # hotfix August 2 2022
+    .metaData = NULL
+) {
+  # Ensure .tsData is provided
+  if (is.null(.tsData)) {
+    stop("Error: .tsData is required but was not provided.")
+  }
 
+  # Convert to data frame if not already
+  .tsData <- as.data.frame(.tsData)
 
+  # Convert specified columns to factors or characters as required
+  .tsData[.compare_categorical] <- lapply(
+    .tsData[.compare_categorical],
+    as.factor
+  )
+  .tsData[.uniqueID_colname] <- lapply(
+    .tsData[.uniqueID_colname],
+    as.character
+  )
 
-lts_input <-  function(.tsData = NULL,
-                       .time = NULL,
-                       .compare_categorical = NULL, #Categorical variables
-                       .plot_measured_variables = FALSE,
-                       .pairedComparisons =  NULL, #pairedVarCCF
-                       .uniqueID_colname = NULL,
-                       .lagMax = NULL, #hotfix july 27 2022
-                       .clusterByPortions = FALSE, #hotfix August 2 2022
-                       .metaData = NULL) {
+  # Create a list of variables
+  lts_variables <- list(
+    lts_data = .tsData,
+    lts_time = .time,
+    lts_compare_by = .compare_categorical,
+    lts_plot_measured_variables = .plot_measured_variables,
+    lts_pariedComparisons = .pairedComparisons,
+    lts_uniqueID_colname = .uniqueID_colname,
+    lts_lagMax = .lagMax, # hotfix July 27 2022
+    lts_clusterByPortions = .clusterByPortions, # hotfix August 2 2022
+    lts_metaData = .metaData
+  )
 
-# lts_input <-  function(.tsData = NULL,
-#                        .time = c("dayOfseason"),
-#                        .compare_categorical = c("season","catchmentRegion"), #Categorical variables
-#                        .plot_measured_variables = FALSE,
-#                        .pairedComparisons = list(
-#                          pair_1 =list(x ="flow_m3s" , y ="rainfall_cm" )), #pairedVarCCF
-#                        .uniqueID_colname = "key_num",
-#                        .metaData = NULL) {
-
-  if(is.null(.tsData)){.tsData <- lts_catchmentsAndRivers}
-
-  .tsData <- as.data.frame(.tsData) #could remove this?
-
-  .tsData[.compare_categorical ] <- lapply(   .tsData[.compare_categorical ] , as.factor) #make compare_by variables, as factors
-  .tsData[.uniqueID_colname ] <- lapply(   .tsData[.uniqueID_colname ] , as.character) #make uniqueID_colname as character #hotxfix feb 20 2022, because uniqueID as numeric cause problems in summaries function
-
-
-  lts_variables <- list(lts_data = .tsData, #create list of variables
-                        lts_time = .time,
-                        lts_compare_by = .compare_categorical,
-                        lts_plot_measured_variables = .plot_measured_variables,
-                        lts_pariedComparisons = .pairedComparisons,
-                        lts_uniqueID_colname = .uniqueID_colname,
-                        lts_lagMax = .lagMax, # hotfix july 27 2-22
-                        lts_clusterByPortions = .clusterByPortions, #hotfix August 2 2022
-                        lts_metaData = .metaData)
-
-  lts_inputVars <-lts_variables
-
-  # lts_tsToWide(lts_inputVars) %>%
-  #   lts_wide_ts_to_ccf(.lts_variables = lts_inputVars) %>%
-  #   lts_ccf_df(.lts_variables = lts_inputVars) %>%
-  #   lts_metaData_ccf_join(.lts_variables = lts_inputVars) %>%
-  #   lts_clusterCCFs(.lts_variables = lts_inputVars) %>%
-  #   lts_leadLagCorr_diffs(.lts_variables = lts_inputVars) -> lts_Output
-
-  return(lts_inputVars)
-
+  return(lts_variables)
 }
